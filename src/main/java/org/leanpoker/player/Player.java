372d1427e568751937f22caa2a9f1c4d27800a8c
sutje <manfred.landgraf@adcubum.com>
2017-06-29 14:04:20 +0200

    dritter commit

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,13 +1,17 @@
 package org.leanpoker.player;
 
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 public class Player {
 
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
-        return 1000;
+    	JsonObject jObject = request.getAsJsonObject();
+//    	jObject.get(memberName)
+        return 200;
+
     }
 
     public static void showdown(JsonElement game) {

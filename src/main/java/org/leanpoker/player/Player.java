099db33514ed26a4e5b72ee2d62af419f8533391
MLammersen <MLammersen@ADC-NB10880.intern.cube.ch>
2017-06-29 14:33:55 +0200

    Erste Strategie

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,19 +1,146 @@
 package org.leanpoker.player;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 public class Player {
 
     static final String VERSION = "Default Java folding player";
+    private static Map<String, Integer> rankValues = null;
 
     public static int betRequest(JsonElement request) {
-    	JsonObject jObject = request.getAsJsonObject();
-//    	jObject.get(memberName)
-        return 200;
-
+    	JsonObject root = request.getAsJsonObject();
+    	JsonArray constellation = getConstellation(root);
+    	int valueOfHand = getValueOfConstellation(constellation);
+        return getBetForValueOfHand(root, valueOfHand);
     }
 
     public static void showdown(JsonElement game) {
     }
+    
+    private static int getBetForValueOfHand(JsonObject root, int valueOfHand) {
+    	if (valueOfHand > 1) { // Mindestens ein Zwilling
+    		return getMinRaise(root);
+    	}
+    	return 0;
+    }
+    
+    private static int getMinRaise(JsonObject root) {
+    	 //     current_buy_in - players[in_action][bet] + minimum_raise
+    	int buyIn = root.get("current_buy_in").getAsInt();
+    	int bet = getMyPlayer(root).get("bet").getAsInt();
+    	int minRaise = root.get("minimum_raise").getAsInt();
+    	return buyIn - bet + minRaise;
+    }
+    
+    private static int getValueOfConstellation(JsonArray constellation) {
+    	return constellation.size();
+    }
+    
+    private static JsonArray getConstellation(JsonObject root) {
+    	JsonArray allCards = getAllCards(root);
+    	Map<String, JsonArray> map = new HashMap<>();
+    	Iterator<JsonElement> iterator = allCards.iterator();
+    	while (iterator.hasNext()) {
+    		JsonElement jsonElement = iterator.next();
+    		JsonObject card = jsonElement.getAsJsonObject();
+    		String key = card.get("rank").getAsString();
+    		JsonArray array = map.get(key);
+    		if (array == null) {
+    			array = new JsonArray();
+    			map.put(key, array);
+    		}
+    		array.add(jsonElement);
+    	}
+    	// Arrays interpretieren
+    	JsonArray best = null;
+    	for (JsonArray combination : map.values()) {
+    		if (best == null) {
+    			best = combination;
+    		} else if (combination.size() > best.size()) {
+    			best = combination;
+    		} else if (combination.size() == best.size()) {
+    			String rankBest = getRankOfCard(best.get(0).getAsJsonObject());
+    			String rankComb = getRankOfCard(combination.get(0).getAsJsonObject());
+    			int valueBest = getValueForRank(rankBest);
+    			int valueComb = getValueForRank(rankBest);
+    			if (valueComb > valueBest) {
+    				best = combination; 
+    			}
+    		}
+    	}
+    	return best;
+    }
+    
+    private static int getValueForRank(String rank) {
+    	int value = 0;
+    	Integer integer = getRankValues().get(rank);
+    	if (integer != null) {
+    		value = integer.intValue();
+    	}
+    	return value;
+    }
+    
+    private static Map<String, Integer> getRankValues() {
+    	if (rankValues == null) {
+    		rankValues = new HashMap<>();
+    		rankValues.put("2", 2);
+    		rankValues.put("3", 2);
+    		rankValues.put("4", 2);
+    		rankValues.put("5", 2);
+    		rankValues.put("6", 2);
+    		rankValues.put("7", 2);
+    		rankValues.put("8", 2);
+    		rankValues.put("9", 2);
+    		rankValues.put("10", 2);
+    		rankValues.put("J", 2);
+    		rankValues.put("Q", 2);
+    		rankValues.put("K", 2);
+    		rankValues.put("A", 2);
+    	}
+    	return rankValues;
+    }
+    
+    private static String getSuitOfCard(JsonObject card) {
+    	return card.get("suit").getAsString();
+    }
+    
+    private static String getRankOfCard(JsonObject card) {
+    	return card.get("rank").getAsString();
+    }
+
+    private static JsonArray getPlayers(JsonObject root) {
+    	return root.get("players").getAsJsonArray();
+    }
+    
+    private static int getMyIndex(JsonObject root) {
+    	return root.get("in_action").getAsInt();
+    }
+    
+    private static JsonObject getMyPlayer(JsonObject root) {
+    	JsonArray players = getPlayers(root);
+    	return players.get(getMyIndex(root)).getAsJsonObject();
+    }
+    
+    private static JsonArray getMyCards(JsonObject root) {
+    	JsonObject myPlayer = getMyPlayer(root);
+    	return myPlayer.get("hole_cards").getAsJsonArray();
+    }
+    
+    private static JsonArray getCommunityCards(JsonObject root) {
+    	return root.get("community_cards").getAsJsonArray();    	
+    }
+    
+    private static JsonArray getAllCards(JsonObject root) {
+    	JsonArray allCards = new JsonArray();
+    	allCards.addAll(getMyCards(root));
+    	allCards.addAll(getCommunityCards(root));
+    	return allCards;
+    }
+    
 }
--- src/test/java/org/leanpoker/player/PlayerTest.java
+++ src/test/java/org/leanpoker/player/PlayerTest.java
@@ -14,6 +14,66 @@ public class PlayerTest {
         JsonElement jsonElement = new JsonParser().parse("{\"key1\": \"value1\", \"key2\": \"value2\"}");
 
         assertEquals(0, Player.betRequest(jsonElement));
+    }
+    
+    @Test
+    public void testBetRequestWithData() throws Exception {
+
+        JsonElement jsonElement = new JsonParser().parse(
+        		"{"+
+        	  "\"players\":["+
+        	    "{"+
+        	     "\"name\":\"Player 1\","+
+        	     "\"stack\":1000,"+
+        	      "\"status\":\"active\","+
+        	      "\"bet\":10,"+
+        	      "\"hole_cards\":[],"+
+        	      "\"version\":\"Version name 1\","+
+        	      "\"id\":0"+
+        	    "},"+
+        	    "{"+
+        	      "\"name\":\"Player 2\","+
+        	      "\"stack\":1000,"+
+        	      "\"status\":\"active\","+
+        	      "\"bet\":20,"+
+        	      "\"hole_cards\":["+
+           	   "{" +
+               "\"rank\": \"4\"," +
+               "\"suit\": \"spades\"" +
+           "}" +
+        	      "],"+
+        	      "\"version\":\"Version name 2\","+
+        	      "\"id\":1"+
+        	    "}"+
+        	  "],"+
+        	  "\"tournament_id\":\"550d1d68cd7bd10003000003\","+
+        	  "\"game_id\":\"550da1cb2d909006e90004b1\","+
+        	  "\"round\":0,"+
+        	  "\"bet_index\":0,"+
+        	  "\"small_blind\":10,"+
+        	  "\"orbits\":0,"+
+        	  "\"dealer\":0,"+
+        	  "\"minimum_raise\": 240,"+
+        	  "\"in_action\": 1," +
+        	  "\"community_cards\":["+
+        	   "{" +
+                  "\"rank\": \"4\"," +
+                  "\"suit\": \"spades\"" +
+              "}," +
+              "{" +
+                  "\"rank\": \"A\"," +
+                  "\"suit\": \"hearts\"" +
+              "}," +
+              "{" +
+                  "\"rank\": \"6\"," +
+                  "\"suit\": \"clubs\"" +
+              "}" +
+        	  "],"+
+        	  "\"current_buy_in\":340,"+
+        	  "\"pot\":0"+
+        	"}");
+
+        assertEquals(560, Player.betRequest(jsonElement));
 
     }
 }
